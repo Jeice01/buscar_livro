c1c5685b16a7d5f46bc7fec3647a3565dda3b57d
Jeice Santos <jeice.santos@gmail.com>
2023-11-22 18:02:05 -0300

    Subindo a aplicação para o gitHub

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,58 @@
+import { FormControl } from '@angular/forms';
+import { VolumeInfo, ImageLinks, Item, LivrosResultado } from './../../models/interfaces';
+import { Component } from '@angular/core';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca = new FormControl();
+
+  mensagemError = '';
+  livrosResultados: LivrosResultado;
+
+  constructor(private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo do inicial')),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultados = resultado),
+    catchError(erro => {
+      console.log(erro)
+      return of()
+
+    })
+  )
+
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo do inicial')),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map(resultado => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError(erro => {
+      console.log(erro);
+      return throwError(() => new Error(this.mensagemError = 'Ops, tem algo errado. Recarregue a aplicação'));
+    })
+  )
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+}
